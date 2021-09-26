3085f4c6db0835b2591207fee44818139a45e002
Ragnarok <Deathmetalldrummer@gmail.com>
2021-09-27 01:42:39 +0600

    Intellect Technology

---
+++ source/devel/_layout/_mounting.js
@@ -0,0 +1,11 @@
+//#include("./_header/_header.js");
+//#include("./_footer/_footer.js");
+
+//#include("./_tabs/_tabs.js");
+//#include("./_nav/_nav.js");
+//#include("./_login/_login.js");
+//#include("./_contacts/_contacts.js");
+//#include("./_box/_box.js");
+//#include("./_icon/_icon.js");
+
+//#include("./_nextParticle/_nextParticle.js");
