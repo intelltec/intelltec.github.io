31f589f7c477befa0d1822c2c95353c490a9f3fb
petrosyan.petros.2001 <petrosyan.petros.2001>
2023-03-01 15:36:23 +0400

    intellec-0103

--- source/build/particles.js
+++
@@ -1,8 +0,0 @@
-const particleID = document.querySelector('#particles-js');
-if (particleID) {
-    particlesJS.load('particles-js', './particles.json', function() {
-        console.log('callback - particles.js config loaded');
-    });
-}
-
-
